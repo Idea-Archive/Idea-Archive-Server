e28bd80f10e7c689e65069b83095ec1261192727
JuuuuHong <wnghd720@naver.com>
2023-02-06 20:41:46 +0900

    :tada: start project

---
+++ src/main/java/Idea/Archieve/IdeaArchieve/IdeaArchieveApplication.java
@@ -0,0 +1,13 @@
+package Idea.Archieve.IdeaArchieve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaArchieveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaArchieveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Idea/Archieve/IdeaArchieve/IdeaArchieveApplicationTests.java
@@ -0,0 +1,13 @@
+package Idea.Archieve.IdeaArchieve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaArchieveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
