40cbc5feff5c25d7024b9fbaf94b1cb60d88790a
JuuuuHong <wnghd720@naver.com>
2023-02-07 14:02:30 +0900

    :sparkles: Configuration 설정

--- src/main/java/Idea/Archieve/IdeaArchieve/IdeaArchieveApplication.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/IdeaArchieveApplication.java
@@ -2,8 +2,16 @@ package Idea.Archieve.IdeaArchieve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableCaching
+@EnableConfigurationProperties
+@ConfigurationProperties
+@EnableJpaRepositories
 public class IdeaArchieveApplication {
 
 	public static void main(String[] args) {
