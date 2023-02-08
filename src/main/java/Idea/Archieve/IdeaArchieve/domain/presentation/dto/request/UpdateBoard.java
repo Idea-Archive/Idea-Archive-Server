226c5135de30968192c9a94c4a1df8d1ba872259
JuuuuHong <wnghd720@naver.com>
2023-02-09 01:54:40 +0900

    :lock: 공백 허용 X

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/presentation/dto/request/UpdateBoard.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/presentation/dto/request/UpdateBoard.java
@@ -4,15 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateBoard {
 
+    @NotBlank(message = "제목은 공백을 허용하지 않습니다.")
     private String title;
 
+    @NotBlank(message = "내용은 공백을 허용하지 않습니다.")
     private String content;
 
+    @NotBlank(message = "카테고리는 공백을 허용하지 않습니다.")
     private String category;
 
 }
--- src/main/java/Idea/Archieve/IdeaArchieve/domain/presentation/dto/request/WriteBoard.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/presentation/dto/request/WriteBoard.java
@@ -5,15 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class WriteBoard {
 
+    @NotBlank(message = "제목은 공백을 허용하지 않습니다.")
     private String title;
 
+    @NotBlank(message = "내용은 공백을 허용하지 않습니다.")
     private String content;
 
+    @NotBlank(message = "카테고리은 공백을 허용하지 않습니다.")
     private String category;
 
     public Board toEntity() {
