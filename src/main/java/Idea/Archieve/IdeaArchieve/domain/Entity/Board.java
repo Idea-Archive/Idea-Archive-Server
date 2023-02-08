cb982c9eb36e6642274b6d556660f6c83f706a7d
JuuuuHong <wnghd720@naver.com>
2023-02-08 23:38:17 +0900

    :memo: 게시판 Entity 생성

---
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/Entity/Board.java
@@ -0,0 +1,30 @@
+package Idea.Archieve.IdeaArchieve.domain.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id", nullable = false)
+    private Long boardId;
+
+    @Column(name = "board_title", nullable = false, length = 30)
+    private String title;
+
+    @Column(name = "board_content", nullable = false)
+    private String content;
+
+    @Column(name = "board_category", nullable = false)
+    private String category;
+
+}
