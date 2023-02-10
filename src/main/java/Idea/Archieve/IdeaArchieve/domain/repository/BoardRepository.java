bebb2e61592c7c36092b4ed766c09dad06e51c56
Kim gyeong su <sk62628504@gmail.com>
2023-02-10 15:37:48 +0900

    :sparkles: 게시글 검색 jpa 메서드 생성

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/repository/BoardRepository.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/repository/BoardRepository.java
@@ -3,5 +3,9 @@ package Idea.Archieve.IdeaArchieve.domain.repository;
 import Idea.Archieve.IdeaArchieve.domain.Entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findByTitleContainingAndCategory(String searchKeyword, String category);
+    List<Board> findByTitleContaining(String searchKeyword);
 }
