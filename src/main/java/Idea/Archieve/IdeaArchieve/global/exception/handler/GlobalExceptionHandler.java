429fa5368a8f9422a93ae7c8c0c1c8ba0f69401e
Kim gyeong su <sk62628504@gmail.com>
2023-02-10 16:47:33 +0900

    :sparkles: 핸들러 추가

--- src/main/java/Idea/Archieve/IdeaArchieve/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/global/exception/handler/GlobalExceptionHandler.java
@@ -1,18 +1,33 @@
 package Idea.Archieve.IdeaArchieve.global.exception.handler;
 
+import Idea.Archieve.IdeaArchieve.domain.board.exception.NotExistBoardException;
+import Idea.Archieve.IdeaArchieve.global.exception.ErrorMessage;
+import com.nimbusds.oauth2.sdk.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
-@RestController
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotExistBoardException.class)
+    public ResponseEntity<ErrorMessage> handleNotExistBoardException(HttpServletRequest request , NotExistBoardException exception) {
+        printError(request, exception, exception.getErrorCode().getMessage());
+        ErrorMessage errorMessage = new ErrorMessage(exception.getErrorCode().getMessage(), exception.getErrorCode().getStatus());
+        return new ResponseEntity<>(errorMessage, HttpStatus.valueOf(exception.getErrorCode().getStatus()));
+    }
 
     private void printError(HttpServletRequest request, RuntimeException ex, String message) {
         log.error(request.getRequestURI());
         log.error(message);
         ex.printStackTrace();
     }
+
 
 }
