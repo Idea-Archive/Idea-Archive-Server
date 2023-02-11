28970832ad24c59d91f17f6c6f335d592ab49bba
JuuuuHong <wnghd720@naver.com>
2023-02-12 00:55:49 +0900

    :memo: MemberDetailsService

---
+++ src/main/java/Idea/Archieve/IdeaArchieve/global/security/auth/MemberDetailsService.java
@@ -0,0 +1,23 @@
+package Idea.Archieve.IdeaArchieve.global.security.auth;
+
+import Idea.Archieve.IdeaArchieve.domain.member.Entity.Member;
+import Idea.Archieve.IdeaArchieve.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+        return new MemberDetails(member);
+    }
+}
