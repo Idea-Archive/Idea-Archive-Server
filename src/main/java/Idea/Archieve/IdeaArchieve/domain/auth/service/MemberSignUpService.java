9b3a24b9d46aad327b11a1967611bf55709dca60
JuuuuHong <wnghd720@naver.com>
2023-02-13 01:59:14 +0900

    :sparkles: 회원가입 서비스

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/service/MemberSignUpService.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/service/MemberSignUpService.java
@@ -1,8 +1,11 @@
 package Idea.Archieve.IdeaArchieve.domain.auth.service;
 
+import Idea.Archieve.IdeaArchieve.domain.auth.exception.ExistEmailException;
 import Idea.Archieve.IdeaArchieve.domain.auth.presentation.dto.request.MemberSignUpRequest;
 import Idea.Archieve.IdeaArchieve.domain.auth.repository.RefreshTokenRepository;
+import Idea.Archieve.IdeaArchieve.domain.member.Entity.Member;
 import Idea.Archieve.IdeaArchieve.domain.member.repository.MemberRepository;
+import Idea.Archieve.IdeaArchieve.global.filter.role.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,5 +19,18 @@ public class MemberSignUpService {
 
     public void execute(MemberSignUpRequest memberSignUpRequest) {
 
+        if(memberRepository.existsByEmail(memberSignUpRequest.getEmail())){
+            throw new ExistEmailException("이미 존재하는 이메일입니다.");
+        }
+
+        Member member = Member
+                .builder()
+                .email(memberSignUpRequest.getEmail())
+                .password(passwordEncoder.encode(memberSignUpRequest.getPassword()))
+                .name(memberSignUpRequest.getName())
+                .role(Role.from(memberSignUpRequest.getRole()))
+                .build();
+
+        memberRepository.save(member);
     }
 }
