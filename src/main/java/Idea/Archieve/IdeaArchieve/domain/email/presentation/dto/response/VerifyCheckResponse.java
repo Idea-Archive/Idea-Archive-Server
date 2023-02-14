e669acf0f72de6bc45d3b54a7154ad604a792752
Kim gyeong su <sk62628504@gmail.com>
2023-02-14 20:48:57 +0900

    :memo: 이메일 인증 Response 설정

---
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/email/presentation/dto/response/VerifyCheckResponse.java
@@ -0,0 +1,14 @@
+package Idea.Archieve.IdeaArchieve.domain.email.presentation.dto.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class VerifyCheckResponse {
+
+
+    private boolean check;
+
+}
