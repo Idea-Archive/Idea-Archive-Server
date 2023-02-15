f3b959a5eba04051ac0faa03ada6fa3b0688da09
Kim gyeong su <sk62628504@gmail.com>
2023-02-15 14:17:15 +0900

    :memo: requestDto 비밀번호 패턴 삭제

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/presentation/dto/request/MemberLoginRequest.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/presentation/dto/request/MemberLoginRequest.java
@@ -18,6 +18,5 @@ public class MemberLoginRequest {
 
 
     @NotBlank(message = "비밀번호는 공백을 허용하지 않습니다.")
-    @Pattern(regexp = "(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 특수문자가 1개 이상 포함되어야 하고 글자 수는 8 ~ 16자 사이여야 합니다.")
     private String password;
 }
