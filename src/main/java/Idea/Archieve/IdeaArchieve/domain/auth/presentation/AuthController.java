215b341976f813775011a5156d32031d6361ce13
Kim gyeong su <sk62628504@gmail.com>
2023-02-15 11:45:15 +0900

    :sparkles: 로그인 서비스 api

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/presentation/AuthController.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/presentation/AuthController.java
@@ -1,6 +1,9 @@
 package Idea.Archieve.IdeaArchieve.domain.auth.presentation;
 
+import Idea.Archieve.IdeaArchieve.domain.auth.presentation.dto.request.MemberLoginRequest;
 import Idea.Archieve.IdeaArchieve.domain.auth.presentation.dto.request.MemberSignUpRequest;
+import Idea.Archieve.IdeaArchieve.domain.auth.presentation.dto.response.MemberLoginResponse;
+import Idea.Archieve.IdeaArchieve.domain.auth.service.MemberLoginService;
 import Idea.Archieve.IdeaArchieve.domain.auth.service.MemberSignUpService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,10 +21,16 @@ import javax.validation.Valid;
 public class AuthController {
 
     private final MemberSignUpService memberSignUpService;
+    private final MemberLoginService memberLoginService;
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signUp(@RequestBody @Valid MemberSignUpRequest memberSignUpRequest) {
         memberSignUpService.execute(memberSignUpRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public MemberLoginResponse login(@RequestBody @Valid MemberLoginRequest memberLoginRequest){
+        return memberLoginService.execute(memberLoginRequest);
     }
 }
