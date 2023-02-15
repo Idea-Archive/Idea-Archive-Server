6b06971d04e61b1934a0386d9a92f053b97623be
Kim gyeong su <sk62628504@gmail.com>
2023-02-15 22:45:56 +0900

    :memo: findById jpa메서드 생성

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/repository/RefreshTokenRepository.java
@@ -3,5 +3,10 @@ package Idea.Archieve.IdeaArchieve.domain.auth.repository;
 import Idea.Archieve.IdeaArchieve.domain.auth.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findById(String email);
+
 }
