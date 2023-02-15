475df725ee4fa40699049b4967a1bcd99e38993d
Kim gyeong su <sk62628504@gmail.com>
2023-02-15 11:43:57 +0900

    :memo: 로그인 responseDto 작성

---
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/presentation/dto/response/MemberLoginResponse.java
@@ -0,0 +1,21 @@
+package Idea.Archieve.IdeaArchieve.domain.auth.presentation.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@Builder
+public class MemberLoginResponse {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private String name;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private ZonedDateTime expiredAt;
+}
