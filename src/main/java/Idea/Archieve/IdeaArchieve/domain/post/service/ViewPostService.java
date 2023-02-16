3bc3650e0afb1a64eb60079d74eed682e2cefa9c
JuuuuHong <wnghd720@naver.com>
2023-02-16 19:15:53 +0900

    :art: 예외처리

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/post/service/ViewPostService.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/post/service/ViewPostService.java
@@ -1,6 +1,7 @@
 package Idea.Archieve.IdeaArchieve.domain.post.service;
 
 import Idea.Archieve.IdeaArchieve.domain.post.Entity.Post;
+import Idea.Archieve.IdeaArchieve.domain.post.exception.NotExistPostException;
 import Idea.Archieve.IdeaArchieve.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,12 @@ import java.util.List;
 public class ViewPostService {
 
     private final PostRepository postRepository;
+
+    private void emptyNotice(List<Post> notice) {
+        if (notice.isEmpty()) {
+            throw new NotExistPostException("존재하지 않는 게시글입니다.");
+        }
+    }
 
     public List<Post> ViewPost() {
         return postRepository.findAll();
