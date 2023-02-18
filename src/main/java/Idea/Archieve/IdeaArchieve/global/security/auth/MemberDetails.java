a81791c5dccebffb115c2871ed5594afb071ceff
Kim gyeong su <sk62628504@gmail.com>
2023-02-18 20:31:22 +0900

    :fire: 필요없는거 정리

--- src/main/java/Idea/Archieve/IdeaArchieve/global/security/auth/MemberDetails.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/global/security/auth/MemberDetails.java
@@ -2,13 +2,9 @@ package Idea.Archieve.IdeaArchieve.global.security.auth;
 
 import Idea.Archieve.IdeaArchieve.domain.member.Entity.Member;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
 import java.util.List;
 
 @Getter
