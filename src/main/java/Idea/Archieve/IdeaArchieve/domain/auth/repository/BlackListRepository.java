9380498818a53e51c0e4942539c3cf03596861e1
Kim gyeong su <sk62628504@gmail.com>
2023-02-18 21:20:41 +0900

    :memo: BlackListRepository, RefreshTokenRepository 수정

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/repository/BlackListRepository.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/repository/BlackListRepository.java
@@ -2,6 +2,7 @@ package Idea.Archieve.IdeaArchieve.domain.auth.repository;
 
 import Idea.Archieve.IdeaArchieve.domain.auth.entity.BlackList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface BlackListRepository extends JpaRepository<BlackList, String> {
+public interface BlackListRepository extends CrudRepository<BlackList, String> {
 }
--- src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/auth/repository/RefreshTokenRepository.java
@@ -2,10 +2,11 @@ package Idea.Archieve.IdeaArchieve.domain.auth.repository;
 
 import Idea.Archieve.IdeaArchieve.domain.auth.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 
     Optional<RefreshToken> findById(String email);
 
