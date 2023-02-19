cd86240e8411a6484497c756c268f452d29a87ce
Kim gyeong su <sk62628504@gmail.com>
2023-02-19 11:23:00 +0900

    :bug: ViewByCategoryResponse 로 리턴형식 변경

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/post/service/PostService.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/post/service/PostService.java
@@ -2,9 +2,12 @@ package Idea.Archieve.IdeaArchieve.domain.post.service;
 
 import Idea.Archieve.IdeaArchieve.domain.post.Entity.Post;
 import Idea.Archieve.IdeaArchieve.domain.post.exception.NotExistPostException;
+import Idea.Archieve.IdeaArchieve.domain.post.presentation.dto.request.WritePost;
+import Idea.Archieve.IdeaArchieve.domain.post.presentation.dto.response.ViewByCategoryResponse;
 import Idea.Archieve.IdeaArchieve.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -21,8 +24,8 @@ public class PostService {
     /*
         카테고리를 선택하고 검색했을때
      */
-    public List<Post> SearchPost(String searchKeyword, String category){
-        List<Post> posts = postRepository.findByTitleContainingAndCategory(searchKeyword, category);
+    public List<ViewByCategoryResponse> SearchPost(String searchKeyword, String category){
+        List<ViewByCategoryResponse> posts = postRepository.findByTitleContainingAndCategory(searchKeyword, category);
         if (posts.size() == 0) {
             throw new NotExistPostException("게시글이 존재하지 않습니다.");
         }
@@ -31,16 +34,17 @@ public class PostService {
     /*
         카테고리를 선택하지 않고 검색했을때
      */
-    public List<Post> SearchPost(String searchKeyword) {
-        List<Post> posts = postRepository.findByTitleContaining(searchKeyword);
+
+    public List<ViewByCategoryResponse> SearchPost(String searchKeyword) {
+        List<ViewByCategoryResponse> posts = postRepository.findByTitleContaining(searchKeyword);
         if (posts.size() == 0) {
             throw new NotExistPostException("게시글이 존재하지 않습니다.");
         }
         return posts;
     }
 
-    public List<Post> viewPostByCategory(String category){
-        List<Post> posts = postRepository.findByCategory(category);
+    public List<ViewByCategoryResponse> viewPostByCategory(String category){
+        List<ViewByCategoryResponse> posts = postRepository.findByCategory(category);
         if (posts.size()==0){
             throw new NotExistPostException("게시글이 존재하지 않습니다.");
         }
