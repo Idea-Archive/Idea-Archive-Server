1e04349da0a05abbfb6c0edbb8970ae6e57d5a69
JuuuuHong <wnghd720@naver.com>
2023-02-20 14:52:49 +0900

    :recycle: UpdatePost 수정중 막힘

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/post/service/UpdatePostService.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/post/service/UpdatePostService.java
@@ -2,14 +2,12 @@ package Idea.Archieve.IdeaArchieve.domain.post.service;
 
 import Idea.Archieve.IdeaArchieve.domain.post.Entity.Post;
 import Idea.Archieve.IdeaArchieve.domain.post.exception.NotExistPostException;
-import Idea.Archieve.IdeaArchieve.domain.post.exception.NotVerifyMember;
+import Idea.Archieve.IdeaArchieve.domain.post.exception.NotMyPost;
 import Idea.Archieve.IdeaArchieve.domain.post.presentation.dto.request.UpdatePost;
 import Idea.Archieve.IdeaArchieve.domain.post.repository.PostRepository;
 import Idea.Archieve.IdeaArchieve.global.util.MemberUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,17 +16,20 @@ public class UpdatePostService {
     private final PostRepository postRepository;
     private final MemberUtil memberUtil;
 
-    private void checkUser(Post post) {
-        if(!memberUtil.currentMember().equals(post.getMember()))
-            throw new NotVerifyMember("검증되지 않은 회원입니다.");
+    private void verifyPostWriter(Post post) {
+        if (!memberUtil.currentMember().equals(post.getMember())) {
+            System.out.println(memberUtil.currentMember());
+            System.out.println(post.getMember());
+            throw new NotMyPost("남의 글은 수정할 수 없습니다.");
+        }
     }
 
     public void execute(Long postId, UpdatePost updatePost) {
-        Post findPost = postRepository.findById(postId)
+        Post post = postRepository.findById(postId)
                         .orElseThrow(() -> new NotExistPostException("존재하지 않는 게시글입니다."));
-        checkUser(findPost);
-        findPost.update(updatePost.getTitle(), updatePost.getContent(), updatePost.getCategory());
-        postRepository.save(findPost);
+        verifyPostWriter(post);
+        post.update(updatePost.getTitle(), updatePost.getContent(), updatePost.getCategory());
+        postRepository.save(post);
     }
 
 }
