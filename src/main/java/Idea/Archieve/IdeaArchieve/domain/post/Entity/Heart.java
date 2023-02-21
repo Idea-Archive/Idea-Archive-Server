3cd102885d160959dd2e3f7b446547ea727e256f
Kim gyeong su <sk62628504@gmail.com>
2023-02-21 21:32:45 +0900

    :memo: 하트 entity 작성

---
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/post/Entity/Heart.java
@@ -0,0 +1,32 @@
+package Idea.Archieve.IdeaArchieve.domain.post.Entity;
+
+
+import Idea.Archieve.IdeaArchieve.domain.member.Entity.Member;
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Heart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "heart_id")
+    private Long heartId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+}
