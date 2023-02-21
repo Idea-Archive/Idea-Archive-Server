38c98817cb8f7305931c6948ac4a8b9db199df0d
Kim gyeong su <sk62628504@gmail.com>
2023-02-21 13:29:32 +0900

    :memo: existsHeartByMemberAndPost 메서드 생성

---
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/post/repository/HeartRepository.java
@@ -0,0 +1,10 @@
+package Idea.Archieve.IdeaArchieve.domain.post.repository;
+
+import Idea.Archieve.IdeaArchieve.domain.post.Entity.Heart;
+import Idea.Archieve.IdeaArchieve.domain.member.Entity.Member;
+import Idea.Archieve.IdeaArchieve.domain.post.Entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HeartRepository extends JpaRepository<Heart, Long> {
+    boolean existsHeartByMemberAndPost(Member member, Post post);
+}
