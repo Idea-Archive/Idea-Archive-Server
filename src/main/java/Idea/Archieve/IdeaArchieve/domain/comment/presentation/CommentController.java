4961c7e64ec409280c579c7fac7a455c35a9ecf0
JuuuuHong <wnghd720@naver.com>
2023-02-21 22:58:33 +0900

    :memo: 댓글 삭제 api

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/comment/presentation/CommentController.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/comment/presentation/CommentController.java
@@ -2,11 +2,13 @@ package Idea.Archieve.IdeaArchieve.domain.comment.presentation;
 
 import Idea.Archieve.IdeaArchieve.domain.comment.presentation.dto.request.ModifyCommentRequest;
 import Idea.Archieve.IdeaArchieve.domain.comment.presentation.dto.request.WriteCommentRequest;
+import Idea.Archieve.IdeaArchieve.domain.comment.service.DeleteCommentService;
 import Idea.Archieve.IdeaArchieve.domain.comment.service.ModifyCommentService;
 import Idea.Archieve.IdeaArchieve.domain.comment.service.WriteCommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,6 +20,7 @@ public class CommentController {
 
     private final WriteCommentService writeCommentService;
     private final ModifyCommentService modifyCommentService;
+    private final DeleteCommentService deleteCommentService;
 
     @PostMapping("/comment/{postId}/write")
     public ResponseEntity<Void> writeComment(@PathVariable Long postId, @RequestBody @Valid WriteCommentRequest request) {
@@ -30,4 +33,11 @@ public class CommentController {
         modifyCommentService.execute(commentId, request);
         return ResponseEntity.ok().build();
     }
+
+    @DeleteMapping("/comment/{commentId}")
+    public ResponseEntity<Void> DeleteComment(@PathVariable Long commentId) {
+        deleteCommentService.execute(commentId);
+        return ResponseEntity.ok().build();
+    }
+
 }
