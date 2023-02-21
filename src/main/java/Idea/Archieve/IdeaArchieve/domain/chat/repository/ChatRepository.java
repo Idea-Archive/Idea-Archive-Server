3031b339beb2a1b7bfdb9ce75dda4cf0625c1b14
JuuuuHong <wnghd720@naver.com>
2023-02-21 17:48:10 +0900

    :art: 패키지 위치 실수

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/chat/repository/ChatRepository.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/chat/repository/ChatRepository.java
@@ -1,4 +1,4 @@
-package Idea.Archieve.IdeaArchieve.domain.chat.entity.repository;
+package Idea.Archieve.IdeaArchieve.domain.chat.repository;
 
 import Idea.Archieve.IdeaArchieve.domain.chat.entity.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
