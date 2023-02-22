b6d004139518df7628505ed9fffd6a3c8107aee2
Kim gyeong su <sk62628504@gmail.com>
2023-02-22 21:10:30 +0900

    :sparkles: deleteByMember_MemberId,findByMember_MemberId 메서드 생성

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/post/repository/HeartRepository.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/post/repository/HeartRepository.java
@@ -5,6 +5,7 @@ import Idea.Archieve.IdeaArchieve.domain.post.Entity.Heart;
 import Idea.Archieve.IdeaArchieve.domain.post.Entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
@@ -12,4 +13,7 @@ public interface HeartRepository extends JpaRepository<Heart, Long> {
     boolean existsHeartByMemberAndPost(Member member , Post post);
 
     Optional<Heart> deleteHeartByMemberAndPost(Member member, Post post);
+
+    void deleteByMember_MemberId(Long memberId);
+    List<Heart> findByMember_MemberId(Long memberId);
 }
