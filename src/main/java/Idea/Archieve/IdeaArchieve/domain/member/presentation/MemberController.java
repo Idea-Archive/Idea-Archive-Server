24a0e8013cf1f5220715472dc9ca66e4dbf23c33
Kim gyeong su <sk62628504@gmail.com>
2023-02-25 16:56:26 +0900

    :sparkles: 나의 게시글목록 보기 api

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/member/presentation/MemberController.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/member/presentation/MemberController.java
@@ -3,14 +3,18 @@ package Idea.Archieve.IdeaArchieve.domain.member.presentation;
 
 import Idea.Archieve.IdeaArchieve.domain.member.presentation.dto.request.ChangePasswordRequest;
 import Idea.Archieve.IdeaArchieve.domain.member.presentation.dto.response.MyPageResponse;
+import Idea.Archieve.IdeaArchieve.domain.member.presentation.dto.response.MyPostResponse;
 import Idea.Archieve.IdeaArchieve.domain.member.service.ChangePasswordService;
 import Idea.Archieve.IdeaArchieve.domain.member.service.MyPageService;
+import Idea.Archieve.IdeaArchieve.domain.member.service.ViewMyPostService;
 import Idea.Archieve.IdeaArchieve.domain.member.service.WithdrawService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +24,7 @@ public class MemberController {
     private final ChangePasswordService changePasswordService;
     private final MyPageService myPageService;
     private final WithdrawService withdrawService;
+    private final ViewMyPostService viewMyPostService;
 
     @PatchMapping
     public ResponseEntity<Void> editPassword(@RequestBody @Valid ChangePasswordRequest changePasswordRequest){
@@ -37,5 +42,10 @@ public class MemberController {
     public ResponseEntity<Void> withdraw(@RequestParam String email,@RequestParam String password){
         withdrawService.execute(email,password);
         return ResponseEntity.noContent().build();
+    }
+    @GetMapping("/myPost")
+    public ResponseEntity<List<MyPostResponse>> viewMyPost(){
+        List<MyPostResponse> myPostResponses = viewMyPostService.execute();
+        return ResponseEntity.ok().body(myPostResponses);
     }
 }
