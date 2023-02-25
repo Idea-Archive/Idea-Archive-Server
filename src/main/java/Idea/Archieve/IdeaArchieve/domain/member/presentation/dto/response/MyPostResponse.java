48b75a2374f1fb087de508d037bb0c991b2c19c0
Kim gyeong su <sk62628504@gmail.com>
2023-02-25 16:55:53 +0900

    :sparkles: 나의 게시글목록 보기 response

---
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/member/presentation/dto/response/MyPostResponse.java
@@ -0,0 +1,34 @@
+package Idea.Archieve.IdeaArchieve.domain.member.presentation.dto.response;
+
+import Idea.Archieve.IdeaArchieve.domain.post.entity.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MyPostResponse {
+
+    private String title;
+    private String category;
+
+    public static MyPostResponse convertToComment(Post post) {
+        return MyPostResponse.builder()
+                .title(post.getTitle())
+                .category(post.getCategory())
+                .build();
+    }
+
+    public static List<MyPostResponse> convertToCommentList(List<Post> commentList) {
+        Stream<Post> stream = commentList.stream();
+        return stream.map(MyPostResponse::convertToComment)
+                .collect(Collectors.toList());
+    }
+}
