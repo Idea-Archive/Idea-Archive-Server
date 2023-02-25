cbf81ee492e5a1f49d3ae0ecc8c0102b32103b93
Kim gyeong su <sk62628504@gmail.com>
2023-02-25 16:56:08 +0900

    :sparkles: 나의 게시글목록 보기 서비스 구현

---
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/member/service/ViewMyPostService.java
@@ -0,0 +1,31 @@
+package Idea.Archieve.IdeaArchieve.domain.member.service;
+
+
+import Idea.Archieve.IdeaArchieve.domain.member.entity.Member;
+import Idea.Archieve.IdeaArchieve.domain.member.presentation.dto.response.MyPostResponse;
+import Idea.Archieve.IdeaArchieve.domain.post.exception.NotExistPostException;
+import Idea.Archieve.IdeaArchieve.global.util.MemberUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ViewMyPostService {
+
+    private final MemberUtil memberUtil;
+
+    @Transactional
+    public List<MyPostResponse> execute(){
+        Member member = memberUtil.currentMember();
+        List<MyPostResponse> posts = MyPostResponse.convertToCommentList(member.getPost());
+        if (posts.size()==0){
+            throw new NotExistPostException("게시글이 존재하지 않습니다.");
+        }
+        return posts;
+    }
+}
