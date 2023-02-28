82aaf2a62307a5df620690896cdd6caecad90f53
JuuuuHong <wnghd720@naver.com>
2023-02-28 19:21:07 +0900

    :sparkles: GoogleInfoClient

---
+++ src/main/java/Idea/Archive/IdeaArchive/infrastructure/client/GoogleAuth.java
@@ -0,0 +1,14 @@
+package Idea.Archive.IdeaArchive.infrastructure.client;
+
+import Idea.Archive.IdeaArchive.infrastructure.dto.request.GoogleCodeRequest;
+import Idea.Archive.IdeaArchive.infrastructure.dto.response.TokenResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@FeignClient(name = "GoogleAuthClient", url = "https://oauth2.googleapis.com/token")
+public interface GoogleAuth {
+
+    @PostMapping
+    TokenResponse googleAuth(GoogleCodeRequest googleCodeRequest);
+
+}
--- src/main/java/Idea/Archive/IdeaArchive/infrastructure/client/GoogleInfo.java
+++ src/main/java/Idea/Archive/IdeaArchive/infrastructure/client/GoogleInfo.java
@@ -1,13 +1,14 @@
 package Idea.Archive.IdeaArchive.infrastructure.client;
 
-import Idea.Archive.IdeaArchive.infrastructure.dto.request.GoogleCodeRequest;
-import Idea.Archive.IdeaArchive.infrastructure.dto.response.TokenResponse;
+import Idea.Archive.IdeaArchive.infrastructure.dto.response.GoogleInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "GoogleAuthClient", url = "https://oauth2.googleapis.com/token")
+@FeignClient(name = "GoogleInfoClient", url = "https://www.googleapis.com/oauth2/v1/userinfo")
 public interface GoogleInfo {
 
-    @PostMapping
-    TokenResponse googleAuth(GoogleCodeRequest googleCodeRequest);
+    @GetMapping("?alt=json&access_token={ACCESS_TOKEN}")
+    GoogleInfoResponse googleInfo(@PathVariable("ACCESS_TOKEN") String accessToken);
+
 }
