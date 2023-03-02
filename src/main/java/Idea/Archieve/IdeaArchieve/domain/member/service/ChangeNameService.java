b9adc59001ea2acb5bec79db61b18084b98aa775
Kim gyeong su <sk62628504@gmail.com>
2023-03-02 17:09:09 +0900

    :sparkles: 예외처리 서비스 구현

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/member/service/ChangeNameService.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/member/service/ChangeNameService.java
@@ -1,7 +1,9 @@
 package Idea.Archieve.IdeaArchieve.domain.member.service;
 
 import Idea.Archieve.IdeaArchieve.domain.member.entity.Member;
+import Idea.Archieve.IdeaArchieve.domain.member.exception.AlreadyExistNicknameException;
 import Idea.Archieve.IdeaArchieve.domain.member.presentation.dto.request.ChangeNameRequest;
+import Idea.Archieve.IdeaArchieve.domain.member.repository.MemberRepository;
 import Idea.Archieve.IdeaArchieve.global.util.MemberUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChangeNameService {
 
     private final MemberUtil memberUtil;
-
+    private final MemberRepository memberRepository;
 
     @Transactional
     public void execute(ChangeNameRequest changeNameRequest){
         Member member = memberUtil.currentMember();
+        if (memberRepository.existsByName(changeNameRequest.getName())){
+            throw new AlreadyExistNicknameException("이미 존재하는 닉네임입니다");
+        }
         member.updateName(changeNameRequest.getName());
     }
 }
