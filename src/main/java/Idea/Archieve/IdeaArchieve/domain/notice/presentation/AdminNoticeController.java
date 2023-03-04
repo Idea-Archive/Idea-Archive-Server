3fcfb628c77ba7c51d9ba04a79f84ba81180ddaf
Kim gyeong su <sk62628504@gmail.com>
2023-03-04 14:52:02 +0900

    :recycle: @Controller -> @RestController 수정

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/notice/presentation/AdminNoticeController.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/notice/presentation/AdminNoticeController.java
@@ -7,16 +7,12 @@ import Idea.Archieve.IdeaArchieve.domain.notice.service.WriteNoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin")
 public class AdminNoticeController {
