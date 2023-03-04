a28ed681c366ccdff94b3860472840527ef0a4c8
Kim gyeong su <sk62628504@gmail.com>
2023-03-04 15:24:45 +0900

    :sparkles: ADMIN 공지글 삭제 api

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/notice/presentation/AdminNoticeController.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/notice/presentation/AdminNoticeController.java
@@ -2,6 +2,7 @@ package Idea.Archieve.IdeaArchieve.domain.notice.presentation;
 
 import Idea.Archieve.IdeaArchieve.domain.notice.presentation.dto.request.WriteNoticeRequest;
 import Idea.Archieve.IdeaArchieve.domain.notice.presentation.dto.response.ViewNoticeResponse;
+import Idea.Archieve.IdeaArchieve.domain.notice.service.DeleteNoticeService;
 import Idea.Archieve.IdeaArchieve.domain.notice.service.ViewNoticeService;
 import Idea.Archieve.IdeaArchieve.domain.notice.service.WriteNoticeService;
 import lombok.RequiredArgsConstructor;
@@ -14,22 +15,26 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/admin")
+@RequestMapping("/admin/notice")
 public class AdminNoticeController {
 
     private final WriteNoticeService writeNoticeService;
     private final ViewNoticeService viewNoticeService;
+    private final DeleteNoticeService deleteNoticeService;
 
     @PostMapping("/write")
     public ResponseEntity<Void> writeNotice(@RequestBody @Valid WriteNoticeRequest writeNoticeRequest){
         writeNoticeService.execute(writeNoticeRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-
     @GetMapping
     public ResponseEntity<List<ViewNoticeResponse>> viewNotice(){
         List<ViewNoticeResponse> viewNoticeResponses = viewNoticeService.execute();
         return ResponseEntity.ok(viewNoticeResponses);
     }
-
+    @DeleteMapping("{noticeId}")
+    public ResponseEntity<Void> deleteNotice(@PathVariable Long noticeId){
+        deleteNoticeService.execute(noticeId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
