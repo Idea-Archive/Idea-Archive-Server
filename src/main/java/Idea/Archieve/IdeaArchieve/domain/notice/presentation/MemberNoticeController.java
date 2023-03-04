5394eb7e7c8352455902ad15d2d8e62ffd761592
Kim gyeong su <sk62628504@gmail.com>
2023-03-04 16:54:00 +0900

    :sparkles: MEMBER viewId별 공지글 보기 api

--- src/main/java/Idea/Archieve/IdeaArchieve/domain/notice/presentation/MemberNoticeController.java
+++ src/main/java/Idea/Archieve/IdeaArchieve/domain/notice/presentation/MemberNoticeController.java
@@ -1,10 +1,13 @@
 package Idea.Archieve.IdeaArchieve.domain.notice.presentation;
 
+import Idea.Archieve.IdeaArchieve.domain.notice.presentation.dto.response.ViewNoticeByIdResponse;
 import Idea.Archieve.IdeaArchieve.domain.notice.presentation.dto.response.ViewNoticeResponse;
+import Idea.Archieve.IdeaArchieve.domain.notice.service.ViewNoticeByIdService;
 import Idea.Archieve.IdeaArchieve.domain.notice.service.ViewNoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +18,16 @@ import java.util.List;
 @RequestMapping("/member/notice")
 public class MemberNoticeController {
     private final ViewNoticeService viewNoticeService;
+    private final ViewNoticeByIdService viewNoticeByIdService;
 
-    @GetMapping("/view")
+    @GetMapping
     public ResponseEntity<List<ViewNoticeResponse>> viewNotice(){
         List<ViewNoticeResponse> viewNoticeResponses = viewNoticeService.execute();
+        return ResponseEntity.ok(viewNoticeResponses);
+    }
+    @GetMapping("{noticeId}")
+    public ResponseEntity<ViewNoticeByIdResponse> viewNoticeById(@PathVariable Long noticeId){
+        ViewNoticeByIdResponse viewNoticeResponses = viewNoticeByIdService.execute(noticeId);
         return ResponseEntity.ok(viewNoticeResponses);
     }
 }
