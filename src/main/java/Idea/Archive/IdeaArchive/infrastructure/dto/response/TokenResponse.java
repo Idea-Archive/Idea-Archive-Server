c36aeb34b74206ef9067eca2680916f35ac974d4
JuuuuHong <wnghd720@naver.com>
2023-03-11 21:39:10 +0900

    :recycle: dto 수정

--- src/main/java/Idea/Archive/IdeaArchive/infrastructure/dto/response/TokenResponse.java
+++ src/main/java/Idea/Archive/IdeaArchive/infrastructure/dto/response/TokenResponse.java
@@ -1,22 +1,13 @@
 package Idea.Archive.IdeaArchive.infrastructure.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 
 @Getter
 @NoArgsConstructor
 public class TokenResponse {
 
     private String accessToken;
-    private String refreshToken;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private ZonedDateTime expiredAt;
 
 }
