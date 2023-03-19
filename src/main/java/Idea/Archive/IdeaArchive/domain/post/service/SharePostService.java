7ede928224a2b88425913164937ddef70f2c3bff
KimGyeongsuuu <sk62628504@gmail.com>
2023-03-19 13:55:58 +0900

    :sparkles: 게시글 공유 서비스 구현

---
+++ src/main/java/Idea/Archive/IdeaArchive/domain/post/service/SharePostService.java
@@ -0,0 +1,24 @@
+package Idea.Archive.IdeaArchive.domain.post.service;
+
+import Idea.Archive.IdeaArchive.domain.post.entity.Post;
+import Idea.Archive.IdeaArchive.domain.post.exception.NotExistPostException;
+import Idea.Archive.IdeaArchive.domain.post.presentation.dto.response.SharePostResponse;
+import Idea.Archive.IdeaArchive.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class SharePostService {
+    private final PostRepository postRepository;
+
+    @Transactional
+    public SharePostResponse execute(Long postId){
+        postRepository.findById(postId)
+                .orElseThrow(() -> new NotExistPostException("존재하지 않은 게시글 입니다"));
+        return SharePostResponse.builder()
+                .postUrl("http://localhost:8080/post/"+postId).
+                build();
+    }
+}
