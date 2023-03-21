5e07528c7d51073dff5621676a1f287f3e5d0124
KimGyeongsuuu <sk62628504@gmail.com>
2023-03-22 00:39:16 +0900

    :sparkles: 구인구직 신청 취소 api

--- src/main/java/Idea/Archive/IdeaArchive/domain/application/presentation/ApplicationController.java
+++ src/main/java/Idea/Archive/IdeaArchive/domain/application/presentation/ApplicationController.java
@@ -1,23 +1,28 @@
 package Idea.Archive.IdeaArchive.domain.application.presentation;
 
 import Idea.Archive.IdeaArchive.domain.application.service.ApplyApplicationService;
+import Idea.Archive.IdeaArchive.domain.application.service.CancelApplicationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/application")
 public class ApplicationController {
     private final ApplyApplicationService applyApplicationService;
+    private final CancelApplicationService cancelApplicationService;
 
     @PostMapping("/{post_id}")
     public ResponseEntity<Void> applyApplication(@PathVariable Long post_id) {
         applyApplicationService.execute(post_id);
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("/{post_id}")
+    public ResponseEntity<Void> cancelApplication(@PathVariable Long post_id) {
+        cancelApplicationService.execute(post_id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
