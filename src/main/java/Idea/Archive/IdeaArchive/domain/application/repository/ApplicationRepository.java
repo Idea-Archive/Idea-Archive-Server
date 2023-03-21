398e0b91bae0a8e0e83a2837a6070a0b28485cb6
KimGyeongsuuu <sk62628504@gmail.com>
2023-03-22 00:38:49 +0900

    :sparkles: delete jpa 메서드 생성

--- src/main/java/Idea/Archive/IdeaArchive/domain/application/repository/ApplicationRepository.java
+++ src/main/java/Idea/Archive/IdeaArchive/domain/application/repository/ApplicationRepository.java
@@ -1,7 +1,10 @@
 package Idea.Archive.IdeaArchive.domain.application.repository;
 
 import Idea.Archive.IdeaArchive.domain.application.entity.Application;
+import Idea.Archive.IdeaArchive.domain.member.entity.Member;
+import Idea.Archive.IdeaArchive.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ApplicationRepository extends JpaRepository<Application,Long> {
+    void deleteByPostAndMember(Post post, Member member);
 }
