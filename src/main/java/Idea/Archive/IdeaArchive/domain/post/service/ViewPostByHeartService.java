5d236169cdf885c3cc1f805893431bcf57cee0c3
KimGyeongsuuu <sk62628504@gmail.com>
2023-03-22 23:13:27 +0900

    :sparkles: 인기글 보기 서비스 구현(좋아요 많은 순으로 정렬)

---
+++ src/main/java/Idea/Archive/IdeaArchive/domain/post/service/ViewPostByHeartService.java
@@ -0,0 +1,35 @@
+package Idea.Archive.IdeaArchive.domain.post.service;
+
+
+import Idea.Archive.IdeaArchive.domain.post.entity.Post;
+import Idea.Archive.IdeaArchive.domain.post.presentation.dto.response.ViewPostResponse;
+import Idea.Archive.IdeaArchive.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ViewPostByHeartService {
+
+    private final PostRepository postRepository;
+
+    @Transactional
+    public List<ViewPostResponse> execute(){
+        List<Post> posts = postRepository.findAll(Sort.by(Sort.Direction.DESC,"heartCount"));
+        return posts.stream()
+                .map(n -> ViewPostResponse.builder()
+                        .id(n.getPostId())
+                        .title(n.getTitle())
+                        .content(n.getContent())
+                        .category(n.getCategory())
+                        .heartCount(n.getHeartCount())
+                        .commentCount(n.getCommentCount())
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
