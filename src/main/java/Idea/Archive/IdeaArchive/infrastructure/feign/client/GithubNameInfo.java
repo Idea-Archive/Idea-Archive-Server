6e50fc115c5765b5e5986de6731e1dc22029b4d8
JuuuuHong <wnghd720@naver.com>
2023-03-23 14:00:53 +0900

    :sparkles: github 이름 받아오기

--- src/main/java/Idea/Archive/IdeaArchive/infrastructure/feign/client/GithubNameInfo.java
+++ src/main/java/Idea/Archive/IdeaArchive/infrastructure/feign/client/GithubNameInfo.java
@@ -1,15 +1,16 @@
 package Idea.Archive.IdeaArchive.infrastructure.feign.client;
 
-import Idea.Archive.IdeaArchive.infrastructure.feign.dto.response.GithubEmailResponse;
+import Idea.Archive.IdeaArchive.infrastructure.feign.dto.response.GithubNameResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "GithubInfoClient", url = "https://api.github.com/user/emails")
+@FeignClient(name = "GithubInfoClient", url = "https://api.github.com/user")
 @Component
-public interface GithubEmailInfo {
+public interface GithubNameInfo {
 
     @GetMapping("?access_token={ACCESS_TOKEN}")
-    GithubEmailResponse githubEmailInfo(@PathVariable("ACCESS_TOKEN") String accessToken);
+    GithubNameResponse githubNameInfo(@PathVariable("ACCESS_TOKEN") String accessToken);
+
 }
---
+++ src/main/java/Idea/Archive/IdeaArchive/infrastructure/feign/dto/response/GithubNameResponse.java
@@ -0,0 +1,12 @@
+package Idea.Archive.IdeaArchive.infrastructure.feign.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GithubNameResponse {
+
+    private String name;
+
+}
