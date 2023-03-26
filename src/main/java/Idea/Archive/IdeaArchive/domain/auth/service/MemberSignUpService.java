b399aa33a016e8579bd25f16865d375183809404
KimGyeongsuuu <sk62628504@gmail.com>
2023-03-26 23:50:34 +0900

    :sparkles: 회원가입시 같은 이름 예외처리 적용

--- src/main/java/Idea/Archive/IdeaArchive/domain/auth/service/MemberSignUpService.java
+++ src/main/java/Idea/Archive/IdeaArchive/domain/auth/service/MemberSignUpService.java
@@ -6,12 +6,15 @@ import Idea.Archive.IdeaArchive.domain.email.entity.EmailAuth;
 import Idea.Archive.IdeaArchive.domain.email.exception.NotVerifyEmailException;
 import Idea.Archive.IdeaArchive.domain.email.repository.EmailAuthRepository;
 import Idea.Archive.IdeaArchive.domain.member.entity.Member;
+import Idea.Archive.IdeaArchive.domain.member.exception.AlreadyExistNicknameException;
 import Idea.Archive.IdeaArchive.domain.member.repository.MemberRepository;
 import Idea.Archive.IdeaArchive.global.filter.role.Role;
 import Idea.Archive.IdeaArchive.global.img.DefaultImage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.naming.NameAlreadyBoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -32,6 +35,10 @@ public class MemberSignUpService {
 
         if (!emailAuth.getAuthentication()) {
             throw new NotVerifyEmailException("인증되지 않은 이메일입니다.");
+        }
+
+        if(memberRepository.existsByName(memberSignUpRequest.getName())){
+            throw new AlreadyExistNicknameException("이미 존재하는 이름입니다");
         }
 
         Member member = Member
