551190c1bf5aca8bac08d17736c7a94a285af9a2
KimGyeongsuuu <sk62628504@gmail.com>
2023-03-27 20:24:45 +0900

    :sparkles: 게시글 수정시 카테고리도 변경 가능하게 수정

--- src/main/java/Idea/Archive/IdeaArchive/domain/post/presentation/dto/request/ModifyPostRequest.java
+++ src/main/java/Idea/Archive/IdeaArchive/domain/post/presentation/dto/request/ModifyPostRequest.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -19,5 +21,7 @@ public class ModifyPostRequest {
     @NotBlank(message = "내용은 공백을 허용하지 않습니다.")
     @Size(min = 1, max = 1000, message = "내용은 1자 ~ 1000자 내외입니다.")
     private String content;
+    @NotNull
+    private List<String> category;
 
 }
