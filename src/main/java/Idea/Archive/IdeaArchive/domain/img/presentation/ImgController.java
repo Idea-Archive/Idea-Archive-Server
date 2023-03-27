1a8de45e670c129957a369e42c8ec0be55846e58
JuuuuHong <wnghd720@naver.com>
2023-03-28 00:33:24 +0900

    :memo: 프로필 삭제 controller

--- src/main/java/Idea/Archive/IdeaArchive/domain/img/presentation/ImgController.java
+++ src/main/java/Idea/Archive/IdeaArchive/domain/img/presentation/ImgController.java
@@ -1,13 +1,11 @@
 package Idea.Archive.IdeaArchive.domain.img.presentation;
 
 import Idea.Archive.IdeaArchive.domain.img.service.ChangeProfileService;
+import Idea.Archive.IdeaArchive.domain.img.service.DeleteProfileImg;
 import Idea.Archive.IdeaArchive.domain.img.service.UploadProfileImg;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -19,16 +17,23 @@ public class ImgController {
 
     private final UploadProfileImg uploadProfileImg;
     private final ChangeProfileService changeProfileService;
+    private final DeleteProfileImg deleteProfileImg;
 
-    @PostMapping("/profile")
+    @PostMapping
     public ResponseEntity<Void> uploadProfileImg(List<MultipartFile> multipartFiles) {
         uploadProfileImg.execute(multipartFiles);
         return ResponseEntity.ok().build();
     }
 
-    @PatchMapping("/change-profile")
+    @PatchMapping
     public ResponseEntity<Void> changeProfileImg(List<MultipartFile> multipartFiles) {
         changeProfileService.execute(multipartFiles);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteProfileImg() {
+        deleteProfileImg.execute();
         return ResponseEntity.ok().build();
     }
 }
