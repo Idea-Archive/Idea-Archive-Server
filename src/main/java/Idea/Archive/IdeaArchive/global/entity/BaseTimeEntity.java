4d9f425f26076f24b2c9f19f06532116a1304dfc
juhong <juhang720@gmail.com>
2023-04-07 15:11:11 +0900

    :sparkles: BaseTimeEntity

---
+++ src/main/java/Idea/Archive/IdeaArchive/global/entity/BaseTimeEntity.java
@@ -0,0 +1,18 @@
+package Idea.Archive.IdeaArchive.global.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime localDateTime;
+
+}
