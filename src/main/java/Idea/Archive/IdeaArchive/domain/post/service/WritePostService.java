517d4b75ac8f1a8b4e2163871e81cbbf1c5a87f1
KimGyeongsuuu <sk62628504@gmail.com>
2023-04-11 07:40:09 +0900

    :sparkles: 카테고리를 List<String>형태로 입력받아서 List<Category>형태로 converter해서 Post에 저장

--- src/main/java/Idea/Archive/IdeaArchive/domain/post/service/WritePostService.java
+++ src/main/java/Idea/Archive/IdeaArchive/domain/post/service/WritePostService.java
@@ -1,6 +1,7 @@
 package Idea.Archive.IdeaArchive.domain.post.service;
 
 import Idea.Archive.IdeaArchive.domain.member.entity.Member;
+import Idea.Archive.IdeaArchive.domain.post.entity.Category;
 import Idea.Archive.IdeaArchive.domain.post.entity.Post;
 import Idea.Archive.IdeaArchive.domain.post.presentation.dto.request.WritePostRequest;
 import Idea.Archive.IdeaArchive.domain.post.repository.PostRepository;
@@ -8,6 +9,9 @@ import Idea.Archive.IdeaArchive.global.util.MemberUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,10 +21,16 @@ public class WritePostService {
     @Transactional
     public void execute(WritePostRequest writePostRequest) {
         Member currentMember = memberUtil.currentMember();
+        List<String> stringList = writePostRequest.getCategory();
+        List<Category> categoryList = new ArrayList<Category>();
+        for (String s : stringList) {
+            Category enumValue = Enum.valueOf(Category.class, s);
+            categoryList.add(enumValue);
+        }
         Post post = Post.builder()
                 .title(writePostRequest.getTitle())
                 .content(writePostRequest.getContent())
-                .category(writePostRequest.getCategory())
+                .category(categoryList)
                 .member(currentMember)
                 .heartCount(0)
                 .commentCount(0)
