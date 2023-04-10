bd7331c0acd380e73d45fd4f122844de63532d52
KimGyeongsuuu <sk62628504@gmail.com>
2023-04-10 17:56:10 +0900

    :sparkles: for문 말고 jpa 쿼리문으로 현재 로그인된 member와 my-page를 이용하려는 member가 같은지 확인하기

--- src/main/java/Idea/Archive/IdeaArchive/domain/post/repository/PostRepository.java
+++ src/main/java/Idea/Archive/IdeaArchive/domain/post/repository/PostRepository.java
@@ -1,9 +1,11 @@
 package Idea.Archive.IdeaArchive.domain.post.repository;
 
+import Idea.Archive.IdeaArchive.domain.member.entity.Member;
 import Idea.Archive.IdeaArchive.domain.post.entity.Post;
 import Idea.Archive.IdeaArchive.domain.post.presentation.dto.response.ViewByCategoryResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,8 +14,12 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<ViewByCategoryResponse> findByTitleContaining(String searchKeyword);
     List<ViewByCategoryResponse> findByCategory(String category);
     void deleteByMember_MemberId(Long memberId);
-
     @Query("SELECT p FROM Post p ORDER BY p.heartCount + p.views DESC")
     List<Post> findAllOrderByHeartCountPlusViesDesc();
+    List<Post> findByMember(Member member);
+    @Query("SELECT COUNT(*) = 0 " +
+            "FROM Post p " +
+            "WHERE p NOT IN :posts OR p.member <> :member")
+    boolean isAllMembersEqual(@Param("member") Member member, @Param("posts") List<Post> posts);
 
 }
