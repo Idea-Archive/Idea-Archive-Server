659e667946e3f5c4428774c4281ec0c7abe74438
KimGyeongsuuu <sk62628504@gmail.com>
2023-04-11 13:29:35 +0900

    :sparkles: findByAllCategories jpa쿼리 메서드 생성

--- src/main/java/Idea/Archive/IdeaArchive/domain/post/repository/PostRepository.java
+++ src/main/java/Idea/Archive/IdeaArchive/domain/post/repository/PostRepository.java
@@ -1,8 +1,8 @@
 package Idea.Archive.IdeaArchive.domain.post.repository;
 
 import Idea.Archive.IdeaArchive.domain.member.entity.Member;
+import Idea.Archive.IdeaArchive.domain.post.category.Category;
 import Idea.Archive.IdeaArchive.domain.post.entity.Post;
-import Idea.Archive.IdeaArchive.domain.post.presentation.dto.response.ViewByCategoryResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,9 +10,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<ViewByCategoryResponse> findByTitleContainingAndCategory(String searchKeyword, String category);
-    List<ViewByCategoryResponse> findByTitleContaining(String searchKeyword);
-    List<ViewByCategoryResponse> findByCategory(String category);
+    @Query("SELECT p FROM Post p JOIN p.category c WHERE c IN (:category) GROUP BY p HAVING COUNT(DISTINCT c) = :numCategory")
+    List<Post> findByAllCategories(@Param("category") List<Category> category, @Param("numCategory") Long numCategory);
     void deleteByMember_MemberId(Long memberId);
     @Query("SELECT p FROM Post p ORDER BY p.heartCount + p.views DESC")
     List<Post> findAllOrderByHeartCountPlusViesDesc();
