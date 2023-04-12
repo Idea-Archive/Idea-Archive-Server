2b7c4767bafdc2a1f25bb5b2b426100b6c27c1f8
KimGyeongsuuu <sk62628504@gmail.com>
2023-04-12 09:08:48 +0900

    :sparkles: 검색하기 / 카테고리와 함께 검색하기 서비스 구현

---
+++ src/main/java/Idea/Archive/IdeaArchive/domain/post/service/SearchPostService.java
@@ -0,0 +1,57 @@
+package Idea.Archive.IdeaArchive.domain.post.service;
+import Idea.Archive.IdeaArchive.domain.member.presentation.dto.ViewMemberResponse;
+import Idea.Archive.IdeaArchive.domain.post.category.Category;
+import Idea.Archive.IdeaArchive.domain.post.entity.Post;
+import Idea.Archive.IdeaArchive.domain.post.exception.NotExistPostException;
+import Idea.Archive.IdeaArchive.domain.post.presentation.dto.request.CategoryRequest;
+import Idea.Archive.IdeaArchive.domain.post.presentation.dto.response.ViewPostResponse;
+import Idea.Archive.IdeaArchive.domain.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SearchPostService {
+
+    private final PostRepository postRepository;
+
+    @Transactional
+    public List<ViewPostResponse> execute(String searchKeyword,CategoryRequest categoryRequest) {
+        List<Post> posts = new ArrayList<Post>();
+        if(categoryRequest.getCategory().isEmpty()) {
+            posts = postRepository.findByTitleContaining(searchKeyword);
+        }else {
+            List<Category> categoryList = new ArrayList<Category>();
+            for (String s : categoryRequest.getCategory()) {
+                Category enumValue = Enum.valueOf(Category.class, s);
+                categoryList.add(enumValue);
+                System.out.println(enumValue);
+            }
+            List<Post> categories = postRepository.findByAllCategories(categoryList, (long) categoryRequest.getCategory().size());
+            for (Post post : categories) {
+                if(post.getTitle().contains(searchKeyword)){
+                    posts.add(post);
+                }
+            }
+        }
+        if (posts.isEmpty()) {
+            throw new NotExistPostException("게시글이 존재하지 않습니다.");
+        }
+        return posts.stream()
+                .map(n -> ViewPostResponse.builder()
+                        .id(n.getPostId())
+                        .title(n.getTitle())
+                        .content(n.getContent())
+                        .category(n.getCategory())
+                        .heartCount(n.getHeartCount())
+                        .commentCount(n.getCommentCount())
+                        .member(ViewMemberResponse.convertToMember(n.getMember()))
+                        .build())
+                .collect(Collectors.toList());
+    }
+}
