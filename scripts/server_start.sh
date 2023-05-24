bfabffd5126623e7994011963225eef97447f300
노가성 <82383294+NohGaSeong@users.noreply.github.com>
2023-05-24 19:44:09 +0900

    :recycle: :: nohup 경로 변경

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -2,4 +2,4 @@
 cd /home/ubuntu/build
 chmod +x ./gradlew
 ./gradlew bootjar
-nohup java -jar build/libs/IdeaArchieve-0.0.1-SNAPSHOT.jar & > /dev/null
+nohup java -jar build/libs/IdeaArchieve-0.0.1-SNAPSHOT.jar > build/nohup.out 2>&1 &
