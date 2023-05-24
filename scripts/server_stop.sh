30a462ee16539d5dad5229591682b5fbf72aa37b
노가성 <82383294+NohGaSeong@users.noreply.github.com>
2023-05-24 20:22:53 +0900

    :recycle: :: 변수들 사용해서 스크립트 변경

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-sudo pkill -f 'java -jar'
+ROOT_PATH="/home/ubuntu/build"
+JAR="$ROOT_PATH/application.jar"
+STOP_LOG="$ROOT_PATH/stop.log"
+SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+
+if [ -z "$SERVICE_PID" ]; then
+  echo "서비스 NouFound" >> $STOP_LOG
+else
+  echo "서비스 종료 " >> $STOP_LOG
+  kill "$SERVICE_PID"
+  # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
+fi
