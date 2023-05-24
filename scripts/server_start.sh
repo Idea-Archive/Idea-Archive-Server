1b508d7d5de7e2864fc7a0de8067133a9fa3c486
노가성 <82383294+NohGaSeong@users.noreply.github.com>
2023-05-24 12:02:58 +0900

    :recycle: :: 경로 변경

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/build
-java -jar IdeaArchieve-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar build/libs/IdeaArchieve-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
