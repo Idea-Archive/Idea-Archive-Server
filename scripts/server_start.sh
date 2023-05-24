3aa58501f339d0dd9643c2c15202a49cd600bd00
노가성 <82383294+NohGaSeong@users.noreply.github.com>
2023-05-24 20:20:45 +0900

    :recycle: :: 변수들 이용해서 스크립트 수정

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
-cd /home/ubuntu/build
-chmod +x ./gradlew
-./gradlew bootjar
-java -jar build/libs/IdeaArchieve-0.0.1-SNAPSHOT.jar
+ROOT_PATH="/home/ubuntu/build"
+JAR="$ROOT_PATH/application.jar"
+
+APP_LOG="$ROOT_PATH/application.log"
+ERROR_LOG="$ROOT_PATH/error.log"
+START_LOG="$ROOT_PATH/start.log"
+
+NOW=$(date +%c)
+
+echo "[$NOW] $JAR 복사" >> $START_LOG
+cp $ROOT_PATH/build/libs/spring-github-action-1.0.0.jar $JAR
+
+echo "[$NOW] > $JAR 실행" >> $START_LOG
+nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+
+SERVICE_PID=$(pgrep -f $JAR)
+echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
