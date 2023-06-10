f46f69c69c657e6044a7140d13e995111d3dae92
KimGyeongsuuu <sk62628504@gmail.com>
2023-06-10 23:22:00 +0900

    :sparkles: 내가 좋아한 글 서비스 구현

--- src/main/java/Idea/Archive/IdeaArchive/domain/member/presentation/dto/response/MyPageResponse.java
+++ src/main/java/Idea/Archive/IdeaArchive/domain/member/presentation/dto/response/MyPageResponse.java
@@ -1,13 +1,8 @@
 package Idea.Archive.IdeaArchive.domain.member.presentation.dto.response;
-
-import Idea.Archive.IdeaArchive.domain.post.presentation.dto.response.ViewPostResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-
 
 @Getter
 @NoArgsConstructor
