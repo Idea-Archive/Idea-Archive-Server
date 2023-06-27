6b10bccaf7c9393260959e8e6da75b5e27cde906
KimGyeongsuuu <sk62628504@gmail.com>
2023-06-27 15:57:42 +0900

    :sparkles: :: healthCheck log 수정

--- src/main/java/Idea/Archive/IdeaArchive/global/health/HealthController.java
+++ src/main/java/Idea/Archive/IdeaArchive/global/health/HealthController.java
@@ -1,7 +1,10 @@
 package Idea.Archive.IdeaArchive.global.health;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +15,7 @@ public class HealthController {
     @GetMapping
     public ResponseEntity<Map<String, String>> healthCheck() {
         Map<String, String> response = new HashMap<>();
-        response.put("message", "IceeTea server running");
+        response.put("message", "IdeaArchive server running");
         return ResponseEntity.ok(response);
     }
 
